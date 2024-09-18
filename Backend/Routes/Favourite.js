1601144f6154a07365653c087c8b033230724037
muktiswarup <csjena1999@gmail.com>
2024-09-18 16:18:00 +0530

    favourits

--- Backend/Routes/Favourite.js
+++ Backend/Routes/Favourite.js
@@ -3,7 +3,6 @@ const User=require('../Model/User')
 const { authenticateToken }=require('./userAuth')
 
 //Add book to Favourite..
-
 router.put("/add-book-to-favourite",authenticateToken ,async (req,res)=>{
     try {
         
@@ -32,5 +31,50 @@ router.put("/add-book-to-favourite",authenticateToken ,async (req,res)=>{
         })
     }
 })
+
+
+//Remove from favourite list
+router.put("/remove-book-from-favourite",authenticateToken, async(req,res)=>{
+    try {
+        const {bookid,id}=req.headers;
+        const userData= await User.findById(id);
+        const isBookFavourite=userData.favourites.includes(bookid)
+        if(isBookFavourite){
+            await User.findByIdAndUpdate(id,{
+                $pull:{favourites:bookid}
+            })
+        }
+        return res.status(200).json({
+            status:"Success",
+            message:"The book succesfully deleted from the favourite list"
+        })
+    } catch (error) {
+        console.log(error)
+        res.status(500).json({
+            status: "failed",
+            message:"Someting error ouccured"
+        })
+    }
+})
+
+
+//get favourite books of a particular user 
+router.get('/get-favourite-books',authenticateToken,async (req,res)=>{
+    try {
+        const {id}= req.headers;
+        const userData=await User.findById(id).populate("favourites");
+        const favouriteBooks=userData.favourites
+        return res.status(200).json({
+            status:"User Favourite books get successfully",
+            data:favouriteBooks
+        })
+    } catch (error) {
+        console.log(error);
+        res.status(500).json({
+            message:"Error occured"
+        })
+    }
+})
+
 
 module.exports=router
