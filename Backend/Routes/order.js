1e193882e875703b6d3558aa33c6611b99dfd801
muktiswarup <csjena1999@gmail.com>
2024-09-27 19:29:03 +0530

    k

---
+++ Backend/Routes/order.js
@@ -0,0 +1,107 @@
+const router=require('express').Router();
+const { authenticateToken }=require('./userAuth')
+const Book=require('../Model/books')
+const Order=require("../Model/order")
+const User=require('../Model/User')
+
+//Place order
+
+router.post("/place-order",authenticateToken, async(req,res)=>{
+    try {
+        const {id}=req.headers;
+        const {order}=req.body;
+        for(const orderData of order){
+            const neworder=new Order({
+                user:id,
+                book:orderData._id
+            })
+            const orderDatafroMDb=await neworder.save();
+            //saving order in usermodel
+            await User.findByIdAndUpdate(id,{
+                $push:{orders:orderDatafroMDb._id}
+            })
+            //clearing cart
+            await User.findByIdAndUpdate(id,{
+                $pull:{carts:orderData._id}
+            })
+            
+        }
+        return res.status(200).json({
+            status:"succss",
+            message:"Order placed succesfully"
+        })
+    } catch (error) {
+        console.log(error)
+        res.status(500).json({
+            messge:"server error occured"
+        })
+    }
+})
+
+//get order history of a particular user
+
+router.get('/get-order-history',authenticateToken, async(req,res)=>{
+    try {
+        const {id}=req.headers;
+        const userdata=await User.findById(id).populate({
+            path: "orders",
+            populate:{path:"book"}
+        })
+        const orderData=userdata.orders.reverse();
+        return res.status(200).json({
+            status:"success",
+            data:orderData
+        })
+    } catch (error) {
+        console.log(error)
+        res.status(500).json({
+            messge:"server error occured"
+        })
+    }
+})
+
+//get all order by admin
+
+router.get("/get-all-orders",authenticateToken,async(req,res)=>{
+    try {
+        const userdata= await Order.find().populate({
+            path:"book"
+        })
+        .populate({
+            path:"user"
+        })
+        .sort({createdAt:-1})
+        res.status(200).json({
+            status:"success",
+            data:userdata
+        })
+    } catch (error) {
+        console.log(error)
+        res.status(500).json({
+            messge:"server error occured"
+        })
+    }
+})
+
+//update order by admin
+
+router.put("/update-status/:id",authenticateToken,async (req,res)=>{
+    try {
+        
+        const {id}=req.params
+        await Order.findByIdAndUpdate(id,
+            {status:req.body.status}
+        )
+        return res.status(200).json({
+            status:"success",
+            message:"status updated successfully"
+        })
+    } 
+    catch (error) {
+        console.log(error)
+        res.status(500).json({
+            messge:"server error occured"
+        })
+    }
+})
+module.exports= router;
--- Backend/index.js
+++ Backend/index.js
@@ -1,10 +1,13 @@
 const express= require('express');
 require('dotenv').config();
 require('./Connection/dbConc')
+
 const User=require('./Routes/user')
 const Books=require('./Routes/book')
 const Favourite= require('./Routes/Favourite')
 const Cart=require("./Routes/cart")
+const Order=require("./Routes/order")
+
 const PORT=process.env.PORT || 1000;
 
 const app= express();
@@ -14,11 +17,10 @@ app.use("/api/v1",User)
 app.use("/api/v1",Books)
 app.use("/api/v1",Favourite)
 app.use("/api/v1",Cart)
+app.use("/api/v1",Order)
 
 
-app.get('/',(req,res)=>{
-    res.send("Hello world");
-})
+
 app.listen(PORT,(err)=>{
     if(err) throw err
     console.log(`The server has started in the ${'http://localhost:3000'}`)
